45853cb73f29998cc81adff71a9b27e5d01b07de
Aarav Shah <as1048@pleasantonusd.net>
2025-02-17 07:55:58 -0800

    finished

--- Board.java
+++ Board.java
@@ -5,121 +5,100 @@
  * @version 2.0
  */
 
-/** 
- * A Board class for concentration
- */
-public class Board
-{  
-  private static String[] tileValues = {"lion", "lion",
-                                        "penguin", "penguin",
-                                        "dolphin", "dolphin",
-                                        "fox", "fox",
-                                        "monkey", "monkey",
-                                        "turtle", "turtle"}; 
-  private Tile[][] gameboard = new Tile[3][4];
 
-  /**  
-   * Constructor for the game. Creates the 2D gameboard
-   * by populating it with card values
-   * 
-   */
-  public Board()
-  {
-   
-    /* your code here */ 
+import java.util.Random;
 
-  }
+public class Board {
+    private static String[] tileValues = { "lion", "lion",
+            "penguin", "penguin",
+            "dolphin", "dolphin",
+            "fox", "fox",
+            "monkey", "monkey",
+            "turtle", "turtle" };
 
- /** 
-   * Returns a string representation of the board, getting the state of
-   * each tile. If the tile is showing, displays its value, 
-   * otherwise displays it as hidden.
-   * 
-   * Precondition: gameboard is populated with tiles
-   * 
-   * @return a string represetation of the board
-   */
-  public String toString()
-  {
- 
-    /* your code here */
- 
-    return "";
-  }
+    private Tile[][] gameboard = new Tile[3][4];
 
-  /** 
-   * Determines if the board is full of tiles that have all been matched,
-   * indicating the game is over.
-   * 
-   * Precondition: gameboard is populated with tiles
-   * 
-   * @return true if all tiles have been matched, false otherwse
-   */
-  public boolean allTilesMatch()
-  {
 
-    /* your code  here */
-    
-    return true;
-  }
+    public Board() {
+        Random rand = new Random();
+        String[] randomizedValues = new String[tileValues.length];
+        boolean[] used = new boolean[tileValues.length];
 
-  /** 
-   * Sets the tile to show its value (like a playing card face up)
-   * 
-   * Preconditions:
-   *   gameboard is populated with tiles,
-   *   row values must be in the range of 0 to gameboard.length,
-   *   column values must be in the range of 0 to gameboard[0].length
-   * 
-   * @param row the row value of Tile
-   * @param column the column value of Tile
-   */
-  public void showValue (int row, int column)
-  {
-   
-    /* your code here */
-  }  
+        for (int i = 0; i < tileValues.length; i++) {
+            int index;
+            do {
+                index = rand.nextInt(tileValues.length); 
+            } while (used[index]); 
 
-  /** 
-   * Checks if the Tiles in the two locations match.
-   * 
-   * If Tiles match, show Tiles in matched state and return a "matched" message
-   * If Tiles do not match, re-hide Tiles (turn face down).
-   * 
-   * Preconditions:
-   *   gameboard is populated with Tiles,
-   *   row values must be in the range of 0 to gameboard.length,
-   *   column values must be in the range of 0 to gameboard[0].length
-   * 
-   * @param row1 the row value of Tile 1
-   * @param col1 the column value of Tile 1
-   * @param row2 the row value of Tile 2
-   * @param col2 the column vlue of Tile 2
-   * @return a message indicating whether or not a match occured
-   */
-  public String checkForMatch(int row1, int col1, int row2, int col2)
-  {
-    String msg = "";
+            randomizedValues[i] = tileValues[index];
+            used[index] = true; 
+        }
 
-     /* your code here */
-    
-     return msg;
-  }
+        int index = 0;
+        for (int row = 0; row < gameboard.length; row++) {
+            for (int col = 0; col < gameboard[row].length; col++) {
+                gameboard[row][col] = new Tile(randomizedValues[index]);
+                index++;
+            }
+        }
 
-  /** 
-   * Checks the provided values fall within the range of the gameboard's dimension
-   * and that the tile has not been previously matched
-   * 
-   * @param rpw the row value of Tile
-   * @param col the column value of Tile
-   * @return true if row and col fall on the board and the row,col tile is unmatched, false otherwise
-   */
-  public boolean validateSelection(int row, int col)
-  {
+        
+    }
 
-    /* your code here */
+    public String toString() {
+        String output = "";
+        for (Tile[] row : gameboard) {
+            for (Tile tile : row) {
+                if (tile.isShowingValue()) {
+                    output = output + tile.getValue() + "\t";
+                } else {
+                    output = output + tile.getHidden() + "\t";
+                }
+            }
+            output = output + "\n";
+        }
+        return output;
+    }
 
-    return true;
-  }
+  
+    public boolean allTilesMatch() {
+        for (Tile[] row : gameboard) {
+            for (Tile tile : row) {
+                if (!tile.matched()) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 
+
+    public void showValue(int row, int column) {
+        if (validateSelection(row, column)) {
+            gameboard[row][column].show();
+        }
+    }
+
+  
+    public String checkForMatch(int row1, int col1, int row2, int col2) {
+        Tile tile1 = gameboard[row1][col1];
+        Tile tile2 = gameboard[row2][col2];
+
+        if (tile1.equals(tile2)) {
+            tile1.foundMatch();
+            tile2.foundMatch();
+            return "It's a match!";
+        } else {
+            tile1.hide();
+            tile2.hide();
+            return "Not a match!";
+        }
+    }
+
+
+    public boolean validateSelection(int row, int col) {
+        return row >= 0 && row < gameboard.length &&
+               col >= 0 && col < gameboard[0].length &&
+               !gameboard[row][col].matched();
+    }
 }
